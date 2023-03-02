31780605babd6d84665acb909d99489fe26163e5
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-03-01 22:41:14 -0800

    Start console version of TTT

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,24 @@
-const gameboard = (() => {
-  const board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+/* eslint-disable no-unused-vars */
+const gameBoardModule = (() => {
+  const board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+  const playRound = (player1, player2) => {
+    console.log(`Tic Tac Toe game started\n${player1.name}'s turn\nSymbol:${player1.symbol}`);
+    prompt(`Where would you like to place '${player1.symbol}'\n[${board[0]} ${board[1]} ${board[2]}]\n[${board[3]} ${board[4]} ${board[5]}]\n[${board[6]} ${board[7]} ${board[8]}]`);
+  };
   return {
-    board,
+    board, playRound,
   };
 })();
 
-const gamePlayer = (name, symbol) => {
-
+const playerFactory = (name, symbol) => {
+  const wins = 0;
+  const losses = 0;
+  const draws = 0;
+  return {
+    name, symbol, wins, losses, draws,
+  };
 };
+
+// Generated players for testing purposes
+const john = playerFactory('John', 'x');
+const brooke = playerFactory('Brooke', 'o');

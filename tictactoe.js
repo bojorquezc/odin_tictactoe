d75bbee233bd5e07a349ca9d9ecb82c5aa184406
Conrado Bojorquez <42232126+bojorquezc@users.noreply.github.com>
2023-03-06 17:07:28 -0800

    Spot taken logic

--- tictactoe.js
+++ tictactoe.js
@@ -12,34 +12,34 @@ const playerFactory = (name, symbol) => {
 };
 
 // Generated players for testing purposes
-const john = playerFactory('John', 'x');
-const adam = playerFactory('Adam', 'o');
+const john = playerFactory('John', 'X');
+const adam = playerFactory('Adam', 'O');
 
 const gameBoardModule = (() => {
   let board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
 
   const winningCondition = () => {
     if (
-      (board[0] === 'x' && board[1] === 'x' && board[2] === 'x')
-      || (board[3] === 'x' && board[4] === 'x' && board[5] === 'x')
-      || (board[6] === 'x' && board[7] === 'x' && board[8] === 'x')
-      || (board[0] === 'x' && board[3] === 'x' && board[6] === 'x')
-      || (board[1] === 'x' && board[4] === 'x' && board[7] === 'x')
-      || (board[2] === 'x' && board[5] === 'x' && board[8] === 'x')
-      || (board[0] === 'x' && board[4] === 'x' && board[8] === 'x')
-      || (board[2] === 'x' && board[4] === 'x' && board[6] === 'x')
+      (board[0] === 'X' && board[1] === 'X' && board[2] === 'X')
+      || (board[3] === 'X' && board[4] === 'X' && board[5] === 'X')
+      || (board[6] === 'X' && board[7] === 'X' && board[8] === 'X')
+      || (board[0] === 'X' && board[3] === 'X' && board[6] === 'X')
+      || (board[1] === 'X' && board[4] === 'X' && board[7] === 'X')
+      || (board[2] === 'X' && board[5] === 'X' && board[8] === 'X')
+      || (board[0] === 'X' && board[4] === 'X' && board[8] === 'X')
+      || (board[2] === 'X' && board[4] === 'X' && board[6] === 'X')
     ) {
       alert('X wins the game');
       board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
     } else if (
-      (board[0] === 'o' && board[1] === 'o' && board[2] === 'o')
-      || (board[3] === 'o' && board[4] === 'o' && board[5] === 'o')
-      || (board[6] === 'o' && board[7] === 'o' && board[8] === 'o')
-      || (board[0] === 'o' && board[3] === 'o' && board[6] === 'o')
-      || (board[1] === 'o' && board[4] === 'o' && board[7] === 'o')
-      || (board[2] === 'o' && board[5] === 'o' && board[8] === 'o')
-      || (board[0] === 'o' && board[4] === 'o' && board[8] === 'o')
-      || (board[2] === 'o' && board[4] === 'o' && board[6] === 'o')
+      (board[0] === 'O' && board[1] === 'O' && board[2] === 'O')
+      || (board[3] === 'O' && board[4] === 'O' && board[5] === 'O')
+      || (board[6] === 'O' && board[7] === 'O' && board[8] === 'O')
+      || (board[0] === 'O' && board[3] === 'O' && board[6] === 'O')
+      || (board[1] === 'O' && board[4] === 'O' && board[7] === 'O')
+      || (board[2] === 'O' && board[5] === 'O' && board[8] === 'O')
+      || (board[0] === 'O' && board[4] === 'O' && board[8] === 'O')
+      || (board[2] === 'O' && board[4] === 'O' && board[6] === 'O')
     ) {
       alert('O wins the game');
       board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
@@ -49,39 +49,44 @@ const gameBoardModule = (() => {
   const playRound = (player1, player2) => {
     const firstPlayer = player1;
     const secondPlayer = player2;
+    const turnCount = 0;
     console.log('Tic Tac Toe game started');
 
     for (let index = 0; index < 5; index += 1) {
       firstPlayer.currentMove = prompt(`${firstPlayer.name} where would you like to place '${firstPlayer.symbol}'?\n[${board[0]} ${board[1]} ${board[2]}]\n[${board[3]} ${board[4]} ${board[5]}]\n[${board[6]} ${board[7]} ${board[8]}]`);
-      board.splice(firstPlayer.currentMove, 1, firstPlayer.symbol);
-      console.log(board);
-      if (
-        (board[0] === 'x' && board[1] === 'x' && board[2] === 'x')
-        || (board[3] === 'x' && board[4] === 'x' && board[5] === 'x')
-        || (board[6] === 'x' && board[7] === 'x' && board[8] === 'x')
-        || (board[0] === 'x' && board[3] === 'x' && board[6] === 'x')
-        || (board[1] === 'x' && board[4] === 'x' && board[7] === 'x')
-        || (board[2] === 'x' && board[5] === 'x' && board[8] === 'x')
-        || (board[0] === 'x' && board[4] === 'x' && board[8] === 'x')
-        || (board[2] === 'x' && board[4] === 'x' && board[6] === 'x')
-      ) {
-        alert(`${firstPlayer.name} wins the game`);
-        board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
-        break;
+      if (board[firstPlayer.currentMove] === 'X' || board[firstPlayer.currentMove] === 'O') {
+        alert('Invalid move, that spot is taken, choose another spot.');
+      } else if (board[firstPlayer.currentMove] !== 'X' || board[firstPlayer.currentMove] !== 'O') {
+        board.splice(firstPlayer.currentMove, 1, firstPlayer.symbol);
+        console.log(board);
+        if (
+          (board[0] === 'X' && board[1] === 'X' && board[2] === 'X')
+          || (board[3] === 'X' && board[4] === 'X' && board[5] === 'X')
+          || (board[6] === 'X' && board[7] === 'X' && board[8] === 'X')
+          || (board[0] === 'X' && board[3] === 'X' && board[6] === 'X')
+          || (board[1] === 'X' && board[4] === 'X' && board[7] === 'X')
+          || (board[2] === 'X' && board[5] === 'X' && board[8] === 'X')
+          || (board[0] === 'X' && board[4] === 'X' && board[8] === 'X')
+          || (board[2] === 'X' && board[4] === 'X' && board[6] === 'X')
+        ) {
+          alert(`${firstPlayer.name} wins the game`);
+          board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+          break;
       }
+    }
 
       secondPlayer.currentMove = prompt(`${secondPlayer.name} where would you like to place '${secondPlayer.symbol}'?\n[${board[0]} ${board[1]} ${board[2]}]\n[${board[3]} ${board[4]} ${board[5]}]\n[${board[6]} ${board[7]} ${board[8]}]`);
       board.splice(secondPlayer.currentMove, 1, secondPlayer.symbol);
       console.log(board);
       if (
-        (board[0] === 'o' && board[1] === 'o' && board[2] === 'o')
-        || (board[3] === 'o' && board[4] === 'o' && board[5] === 'o')
-        || (board[6] === 'o' && board[7] === 'o' && board[8] === 'o')
-        || (board[0] === 'o' && board[3] === 'o' && board[6] === 'o')
-        || (board[1] === 'o' && board[4] === 'o' && board[7] === 'o')
-        || (board[2] === 'o' && board[5] === 'o' && board[8] === 'o')
-        || (board[0] === 'o' && board[4] === 'o' && board[8] === 'o')
-        || (board[2] === 'o' && board[4] === 'o' && board[6] === 'o')
+        (board[0] === 'O' && board[1] === 'O' && board[2] === 'O')
+        || (board[3] === 'O' && board[4] === 'O' && board[5] === 'O')
+        || (board[6] === 'O' && board[7] === 'O' && board[8] === 'O')
+        || (board[0] === 'O' && board[3] === 'O' && board[6] === 'O')
+        || (board[1] === 'O' && board[4] === 'O' && board[7] === 'O')
+        || (board[2] === 'O' && board[5] === 'O' && board[8] === 'O')
+        || (board[0] === 'O' && board[4] === 'O' && board[8] === 'O')
+        || (board[2] === 'O' && board[4] === 'O' && board[6] === 'O')
       ) {
         alert(`${secondPlayer.name} wins the game`);
         board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
@@ -94,3 +99,4 @@ const gameBoardModule = (() => {
     board, winningCondition, playRound,
   };
 })();
+

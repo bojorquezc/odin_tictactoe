4bface0e95e7a384deb4efb22a9d6cc08094c7f7
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-03-15 23:40:21 -0700

    Add turn functionality

--- tictactoe.js
+++ tictactoe.js
@@ -30,129 +30,116 @@ const displayController = (() => {
   const square8 = document.querySelector('.square-8');
   const squareArray = [square0, square1, square2, square3, square4, square5, square6, square7, square8];
 
-  // squareArray.forEach((square) => {
-  //   square.addEventListener('click', () => {
-  //     gameBoardModule.board.forEach(() => {
-  //       if (gameBoardModule.board[squareArray.indexOf(square)] === 'X' || gameBoardModule.board[squareArray.indexOf(square)] === 'O') {
-  //         square.textContent = gameBoardModule.board[squareArray.indexOf(square)];
-  //       }
-  //     });
-  //   });
-  // });
-
   const displayBoard = () => {
-
+    squareArray.forEach((square) => {
+      if (square.textContent !== 'X' || square.textContent !== 'O') {
+        square.textContent = gameBoardModule.board[squareArray.indexOf(square)];
+      }
+    });
   };
 
   return {
-    displayBoard, squareArray,
+    squareArray, displayBoard,
   };
 })();
 
 const gameBoardModule = (() => {
-  let board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+  let board = ['', '', '', '', '', '', '', '', ''];
 
   const playRound = (player1, player2) => {
     const firstPlayer = player1;
     const secondPlayer = player2;
     let turnCount = 0;
 
-    displayController.squareArray.forEach((square) => {
-      square.addEventListener('click', () => {
-        board[displayController.squareArray.indexOf(square)] = firstPlayer.symbol;
-        square.textContent = board[displayController.squareArray.indexOf(square)];
+    const winningCondition = () => {
+      let gameWinner = '';
+      if (
+        (board[0] === 'X' && board[1] === 'X' && board[2] === 'X')
+        || (board[3] === 'X' && board[4] === 'X' && board[5] === 'X')
+        || (board[6] === 'X' && board[7] === 'X' && board[8] === 'X')
+        || (board[0] === 'X' && board[3] === 'X' && board[6] === 'X')
+        || (board[1] === 'X' && board[4] === 'X' && board[7] === 'X')
+        || (board[2] === 'X' && board[5] === 'X' && board[8] === 'X')
+        || (board[0] === 'X' && board[4] === 'X' && board[8] === 'X')
+        || (board[2] === 'X' && board[4] === 'X' && board[6] === 'X')
+      ) {
+        gameWinner = `${firstPlayer.name} wins the game`;
+        console.log(gameWinner);
+        board = ['', '', '', '', '', '', '', '', ''];
+      } else if (
+        (board[0] === 'O' && board[1] === 'O' && board[2] === 'O')
+        || (board[3] === 'O' && board[4] === 'O' && board[5] === 'O')
+        || (board[6] === 'O' && board[7] === 'O' && board[8] === 'O')
+        || (board[0] === 'O' && board[3] === 'O' && board[6] === 'O')
+        || (board[1] === 'O' && board[4] === 'O' && board[7] === 'O')
+        || (board[2] === 'O' && board[5] === 'O' && board[8] === 'O')
+        || (board[0] === 'O' && board[4] === 'O' && board[8] === 'O')
+        || (board[2] === 'O' && board[4] === 'O' && board[6] === 'O')
+      ) {
+        gameWinner = `${secondPlayer.name} wins the game`;
+        console.log(gameWinner);
+        board = ['', '', '', '', '', '', '', '', ''];
+      }
+
+      turnCount += 1;
+      console.log(turnCount);
+      if (turnCount === 9) {
+        console.log('The game is tied');
+        board = ['', '', '', '', '', '', '', '', ''];
+      }
+    };
+
+    const boardClickFirstPlayer = () => {
+      displayController.squareArray.forEach((square) => {
+        square.addEventListener('click', () => {
+          // if (board[displayController.squareArray.indexOf(square)] === 'X'
+          // || board[displayController.squareArray.indexOf(square)] === 'O') {
+          //   console.log('Spot already taken, choose another spot');
+          // }
+          board[displayController.squareArray.indexOf(square)] = firstPlayer.symbol;
+          firstPlayer.currentMove = board[displayController.squareArray.indexOf(square)];
+          displayController.displayBoard();
+          winningCondition();
+          currentTurn = secondPlayer;
+          gameFlow();
+        });
       });
-    });
+    };
 
-    for (let index = 0; index < 5; index += 1) {
-      // firstPlayer.currentMove = prompt(`${firstPlayer.name} where would you like to place '${firstPlayer.symbol}'?\n[${board[0]} ${board[1]} ${board[2]}]\n[${board[3]} ${board[4]} ${board[5]}]\n[${board[6]} ${board[7]} ${board[8]}]`);
+    const boardClickSecondPlayer = () => {
+      displayController.squareArray.forEach((square) => {
+        square.addEventListener('click', () => {
+          // if (board[displayController.squareArray.indexOf(square)] === 'X'
+          // || board[displayController.squareArray.indexOf(square)] === 'O') {
+          //   console.log('Spot already taken, choose another spot');
+          // }
+          board[displayController.squareArray.indexOf(square)] = secondPlayer.symbol;
+          secondPlayer.currentMove = board[displayController.squareArray.indexOf(square)];
+          displayController.displayBoard();
+          winningCondition();
+          currentTurn = firstPlayer;
+          gameFlow();
+        });
+      });
+    };
 
-      while (
-        Number(firstPlayer.currentMove) > 9
-        || Number(firstPlayer.currentMove) < 0
-      ) {
-        alert('Invalid number, choose a valid number.');
-        firstPlayer.currentMove = prompt(`${firstPlayer.name} where would you like to place '${firstPlayer.symbol}'?\n[${board[0]} ${board[1]} ${board[2]}]\n[${board[3]} ${board[4]} ${board[5]}]\n[${board[6]} ${board[7]} ${board[8]}]`);
-      }
+    let currentTurn = firstPlayer;
 
-      while (
-        board[firstPlayer.currentMove] === 'X'
-        || board[firstPlayer.currentMove] === 'O'
-      ) {
-        alert('Invalid move, that spot is taken, choose another spot.');
-        firstPlayer.currentMove = prompt(`${firstPlayer.name} where would you like to place '${firstPlayer.symbol}'?\n[${board[0]} ${board[1]} ${board[2]}]\n[${board[3]} ${board[4]} ${board[5]}]\n[${board[6]} ${board[7]} ${board[8]}]`);
+    const gameFlow = () => {
+      if (currentTurn === firstPlayer) {
+        boardClickFirstPlayer();
+        // winningCondition();
+      } else if (currentTurn === secondPlayer) {
+        boardClickSecondPlayer();
+        // winningCondition();
       }
+    };
 
-      if (board[firstPlayer.currentMove] !== 'X' || board[firstPlayer.currentMove] !== 'O') {
-        board.splice(firstPlayer.currentMove, 1, firstPlayer.symbol);
-        console.log(board);
-        displayController.displayBoard();
-        if (
-          (board[0] === 'X' && board[1] === 'X' && board[2] === 'X')
-          || (board[3] === 'X' && board[4] === 'X' && board[5] === 'X')
-          || (board[6] === 'X' && board[7] === 'X' && board[8] === 'X')
-          || (board[0] === 'X' && board[3] === 'X' && board[6] === 'X')
-          || (board[1] === 'X' && board[4] === 'X' && board[7] === 'X')
-          || (board[2] === 'X' && board[5] === 'X' && board[8] === 'X')
-          || (board[0] === 'X' && board[4] === 'X' && board[8] === 'X')
-          || (board[2] === 'X' && board[4] === 'X' && board[6] === 'X')
-        ) {
-          alert(`${firstPlayer.name} wins the game`);
-          board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
-          break;
-        }
-      }
-      turnCount += 1;
-      if (turnCount === 9) {
-        alert('The game is tied');
-        board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
-        break;
-      }
+    gameFlow();
 
-      secondPlayer.currentMove = prompt(`${secondPlayer.name} where would you like to place '${secondPlayer.symbol}'?\n[${board[0]} ${board[1]} ${board[2]}]\n[${board[3]} ${board[4]} ${board[5]}]\n[${board[6]} ${board[7]} ${board[8]}]`);
-
-      while (
-        Number(secondPlayer.currentMove) > 9
-        || Number(secondPlayer.currentMove) < 0
-      ) {
-        alert('Invalid number, choose a valid number.');
-        secondPlayer.currentMove = prompt(`${secondPlayer.name} where would you like to place '${secondPlayer.symbol}'?\n[${board[0]} ${board[1]} ${board[2]}]\n[${board[3]} ${board[4]} ${board[5]}]\n[${board[6]} ${board[7]} ${board[8]}]`);
-      }
-
-      while (
-        board[secondPlayer.currentMove] === 'X'
-        || board[secondPlayer.currentMove] === 'O'
-      ) {
-        alert('Invalid move, that spot is taken, choose another spot.');
-        secondPlayer.currentMove = prompt(`${secondPlayer.name} where would you like to place '${secondPlayer.symbol}'?\n[${board[0]} ${board[1]} ${board[2]}]\n[${board[3]} ${board[4]} ${board[5]}]\n[${board[6]} ${board[7]} ${board[8]}]`);
-      }
-
-      if (board[secondPlayer.currentMove] !== 'X' || board[secondPlayer.currentMove] !== 'O') {
-        board.splice(secondPlayer.currentMove, 1, secondPlayer.symbol);
-        console.log(board);
-        displayController.displayBoard();
-        if (
-          (board[0] === 'X' && board[1] === 'X' && board[2] === 'X')
-          || (board[3] === 'X' && board[4] === 'X' && board[5] === 'X')
-          || (board[6] === 'X' && board[7] === 'X' && board[8] === 'X')
-          || (board[0] === 'X' && board[3] === 'X' && board[6] === 'X')
-          || (board[1] === 'X' && board[4] === 'X' && board[7] === 'X')
-          || (board[2] === 'X' && board[5] === 'X' && board[8] === 'X')
-          || (board[0] === 'X' && board[4] === 'X' && board[8] === 'X')
-          || (board[2] === 'X' && board[4] === 'X' && board[6] === 'X')
-        ) {
-          alert(`${secondPlayer.name} wins the game`);
-          board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
-          break;
-        }
-      }
-      turnCount += 1;
-      if (turnCount === 9) {
-        alert('The game is tied');
-        board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
-        break;
-      }
-    }
+    return {
+      firstPlayer, secondPlayer,
+    };
   };
 
   return {
